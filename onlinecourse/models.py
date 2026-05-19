7b86aadb4ecdb068484e9234b1204ae50a225be7
Vijay Patil <vijaypatil1601@gmail.com>
2026-05-19 11:21:22 +0530

    Implement Question, Choice, and Submission models
    Added Question, Choice, and Submission models to handle course assessments.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,7 +94,63 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+# Question model
+class Question(models.Model):
 
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE
+    )
+
+    content = models.CharField(
+        max_length=200
+    )
+
+    grade = models.IntegerField(
+        default=0
+    )
+
+    def __str__(self):
+
+        return self.content
+
+
+# Choice model
+class Choice(models.Model):
+
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
+    content = models.CharField(
+        max_length=200
+    )
+
+    is_correct = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+
+        return self.content
+
+
+# Submission model
+class Submission(models.Model):
+
+    enrollment = models.ForeignKey(
+        Enrollment,
+        on_delete=models.CASCADE
+    )
+
+    choices = models.ManyToManyField(
+        Choice
+    )
+
+    def __str__(self):
+
+        return str(self.id)
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
