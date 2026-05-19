d7102a054981acda5d0651966c06cab2ef04bf88
Vijay Patil <vijaypatil1601@gmail.com>
2026-05-19 11:10:33 +0530

    Refactor URL patterns in onlinecourse/urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,24 +1,69 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
-    path('registration/', views.registration_request, name='registration'),
-    path('login/', views.login_request, name='login'),
-    path('logout/', views.logout_request, name='logout'),
+
+    # Home page
+    path(
+        route='',
+        view=views.CourseListView.as_view(),
+        name='index'
+    ),
+
+    # Registration
+    path(
+        'registration/',
+        views.registration_request,
+        name='registration'
+    ),
+
+    # Login
+    path(
+        'login/',
+        views.login_request,
+        name='login'
+    ),
+
+    # Logout
+    path(
+        'logout/',
+        views.logout_request,
+        name='logout'
+    ),
+
+    # Course details
     # ex: /onlinecourse/5/
-    path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
-    path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    path(
+        '<int:pk>/',
+        views.CourseDetailView.as_view(),
+        name='course_details'
+    ),
 
-    # <HINT> Create a route for submit view
+    # Enroll course
+    # ex: /onlinecourse/5/enroll/
+    path(
+        '<int:course_id>/enroll/',
+        views.enroll,
+        name='enroll'
+    ),
 
-    # <HINT> Create a route for show_exam_result view
+    # Submit exam
+    path(
+        '<int:course_id>/submit/',
+        views.submit,
+        name='submit'
+    ),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Show exam result
+    path(
+        'course/<int:course_id>/submission/<int:submission_id>/result/',
+        views.show_exam_result,
+        name='exam_result'
+    ),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
